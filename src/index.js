1004417eeb60b29870db9be23a2f3652cff0b843
Natallia Behlik <natasha-1909@yandex.ru>
2020-03-22 21:31:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let checkArr = str.split('');
+
+    for (let i = 0; i < checkArr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let openBracket = checkArr[i] === bracketsConfig[j][0];
+            let closeBracket = checkArr[i + 1] === bracketsConfig[j][1];
+            if (openBracket && closeBracket) {
+                checkArr.splice(i, 2);
+                i = -1;
+            }
+        }
+
+        if (checkArr.length === 0) {
+            return true;
+        }
+    }
+
+    return false;
+};
